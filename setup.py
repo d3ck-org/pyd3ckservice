414a3482c1e6a3f28c7d1ba637a2811c7d0b967e
d3ck <d3ck.org@gmail.com>
2018-01-03 18:00:09 +0100

    MANIFEST.in added

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 # https://stackoverflow.com/a/20234833
 # https://mike.zwobble.org/2013/05/adding-git-or-hg-or-svn-dependencies-in-setup-py/
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#using-find-packages
+# https://danielsokolowski.blogspot.de/2012/08/setuptools-includepackagedata-option.html
 
 from setuptools import setup, find_packages
 
@@ -11,9 +12,8 @@ setup(
     name='pyd3ckservice',
     version='0.0.1',
     description='Test project, do not use in production!',
-    packages=find_packages(
-        exclude=['examples'],
-        include=['redis', 'mongo', 'flask', 'elasticsearch']),
+    include_package_data=True,  # enable MANIFEST.in
+    packages=find_packages(exclude=['examples']),
     install_requires=['pyd3ckbase>=0.0.1'],
     dependency_links=[
         'git+https://github.com/d3ck-org/pyd3ckbase.git#egg=pyd3ckbase-0.0.1'
